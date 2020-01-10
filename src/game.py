a527eb423f38ebf357c98ca87d59a520d5cf3ab5
oherrera1234 <58004493+oherrera1234@users.noreply.github.com>
2020-01-10 13:20:21 -0500

    master

--- src/game.py
+++ src/game.py
@@ -1,133 +1,43 @@
-import random
+import pygame
+import sys
 
-def winner(board):
-    """This function accepts the Connect 4 board as a parameter.
-    If there is no winner, the function will return the empty string "".
-    If the user has won, it will return 'X', and if the computer has
-    won it will return 'O'."""
+width, height = 600, 600
+cell_count = 7
+cell_size = width/cell_count
 
-    # Check rows for winner
-    for row in range(6):
-        for col in range(3):
-            if (board[row][col] == board[row][col + 1] == board[row][col + 2] ==\
-                board[row][col + 3]) and (board[row][col] != " "):
-                return board[row][col]
+grid = []
 
-    # Check columns for winner
-    for col in range(6):
-        for row in range(3):
-            if (board[row][col] == board[row + 1][col] == board[row + 2][col] ==\
-                board[row + 3][col]) and (board[row][col] != " "):
-                return board[row][col]
+class Cell:
 
-    # Check diagonal (top-left to bottom-right) for winner
+    def __init__(self, x, y, sz):
+        self.x = x
+        self.y = y
+        self.sz = sz
+        self.filled = False
+    
+    def show(self):
+        pygame.draw.rect(screen, pygame.Color("blue"), (self.x * self.sz,self.y * self.sz,self.sz,self.sz), 2)
+        if self.filled:
+            pygame.draw.rect(screen, pygame.Color("red"), (self.x * self.sz, self.y * self.sz, self.sz, self.sz))
 
-    for row in range(3):
-        for col in range(4):
-            if (board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] ==\
-                board[row + 3][col + 3]) and (board[row][col] != " "):
-                return board[row][col]
+pygame.init()
 
+screen = pygame.display.set_mode((width, height))
 
-    # Check diagonal (bottom-left to top-right) for winner
+for y in range(cell_count):
+    for x in range(cell_count):
+        grid.append(Cell(x, y, cell_size))
 
-    for row in range(5, 2, -1):
-        for col in range(3):
-            if (board[row][col] == board[row - 1][col + 1] == board[row - 2][col + 2] ==\
-                board[row - 3][col + 3]) and (board[row][col] != " "):
-                return board[row][col]
+grid[3].filled = True
 
-    # No winner: return the empty string
-    return ""
-
-def display_board(board):
-
-    print ("   1   2   3   4    5   6   7")
-    print ("1: " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " | " + board[0][3] + " | " + board[0][4] + " | " + board[0][5] + " | " + board[0][6] + " | " + board[0][7])
-    print ("  ---+---+---+---+---+---+---")
-    print ("2: " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " | " + board[1][3] + " | " + board[1][4] + " | " + board[1][5] + " | " + board [1][6] + " | " + board [1][7])
-    print ("  ---+---+---+---+---+---+---+")
-    print ("3: " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " | " + board[2][3] + " | " + board [2][4] + " | " + board [2][5] + " | " + board [2][6] + " | " + board [2][7])
-    print ("  ---+---+---+---+---+---+---+")
-    print ("4: " + board[3][0] + " | " + board[3][1] + " | " + board[3][2] + " | " + board[3][3] + " | " + board [3][4] + " | " + board [3][5] + " | " + board [3][6] + " | " + board [3][7])
-    print ("  ---+---+---+---+---+---+---+")
-    print ("5: " + board[4][0] + " | " + board[4][1] + " | " + board[4][2] + " | " + board[4][3] + " | " + board [4][4] + " | " + board [4][5] + " | " + board [4][6] + " | " + board [4][7])
-    print ("  ---+---+---+---+---+---+---+")
-    print ("6: " + board[5][0] + " | " + board[5][1] + " | " + board[5][2] + " | " + board[5][3] + " | " + board [5][4] + " | " + board [5][5] + " | " + board [5][6] + " | " + board [5][7])
-    print
-
-def make_user_move(board):
-
-    try:
-        valid_move = False
-        while not valid_move:
-            col = input("What col would you like to move to (1-7):")
-            for row in range (6,0,-1):
-                if (1 <= row <= 6) and (1 <= col <= 7) and (board[row-1][col-1] == " "):
-                    board[row-1][col-1] = 'X'
-                    valid_move = True
-                    break
-            else:
-                print ("Sorry, invalid square. Please try again!\n")
-
-    except NameError:
-        print ("Only numbers are allowed.")
-
-    except IndexError:
-        print ("You can only select columns from (1-7), and rows from (1-6).")
-
-def make_computer_move(board):
-    # Code needed here...
-    valid_move = False
-    while not valid_move:
-        row = random.randint(0,5)
-        col = random.randint(0, 6)
-        for row in range (5,0,-1):
-            if board[row][col] == " ":
-                board[row][col] = "O"
-                valid_move = True
-                break
-
-def main():
-    free_cells = 42
-    users_turn = True
-    count = 1
-    ttt_board = [ [ " ", " ", " ", " ", " ", " "," ", " "], [ " ", " ", " ", " ", " "," ", " ", " "], [ " ", " ", " ", " ", " ", " ", " ", " "], [ " ", " ", " ", " ", " ", " ", " ", " "], [ " ", " ", " ", " ", " ", " ", " ", " "], [ " ", " ", " ", " ", " ", " ", " ", " "] ]
-
-    choice = input("Would you like to go first? (y or n): ")
-
-    if (choice == 'y' or choice=='Y'):
-        users_turn = True
-
-
-    elif (choice == 'n' or choice =='N') :
-        users_turn = False
-
-    else:
-        print ('invalid input')
-
-    while not winner(ttt_board) and (free_cells > 0):
-        display_board(ttt_board)
-        if users_turn:
-            make_user_move(ttt_board)
-            users_turn = not users_turn
-        else:
-            make_computer_move(ttt_board)
-            users_turn = not users_turn
-        free_cells -= 1
-
-    display_board(ttt_board)
-    if (winner(ttt_board) == 'X'):
-        print ("You Won!")
-        print ("Your name will now be added to the Hall of Fame!")
-
-        print ("\nGAME OVER")
-    elif (winner(ttt_board) == 'O'):
-        print ("The Computer Won!")
-        print ("\nGAME OVER")
-    else:
-        print ("Stalemate!")
-        print ("\nGAME OVER \n")
-
-#start the game
-main()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    
+    # pygame.draw.rect(screen, pygame.Color("red"), (0,0,50,50))
+    for cell in grid:
+        cell.show()
+        
+        
+    pygame.display.flip()
